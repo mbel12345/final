5fddad56ea6157b49bed9b92dcda71737e0e6d4e
Matthew Belanger <msb64@njit.edu>
2026-05-09 23:18:29 -0400

    CalcsPerDay: Models + Schema + Routes

--- tests/e2e/test_statistics_routes.py
+++ tests/e2e/test_statistics_routes.py
@@ -1,7 +1,9 @@
 from fastapi.testclient import TestClient
 
 from app.main import app
+from app.models import CalcsPerDay
 from app.models import Calculation
+from tests.conftest import create_calcs_by_day
 from tests.conftest import midnight_n_days_ago
 from tests.conftest import register_and_login
 
@@ -200,3 +202,268 @@ def test_statistics_total_calcs_start_time_and_end_time_filter(db_session):
     assert data['subtraction'] == 1
     assert data['multiplication'] == 1
     assert data['division'] == 0
+
+
+# ---------------------------------------------------
+# Calculations Per Day
+# ---------------------------------------------------
+
+
+def test_statistics_calcs_per_day_addition(db_session):
+
+    # Test CalcsPerDay with addition
+
+    # Create calculations
+    user_data = register_and_login(client)
+    create_calcs_by_day(user_data, db_session, client)
+
+    # Get calculations
+    access_token = user_data['access_token']
+    headers = {'Authorization': f'Bearer {access_token}'}
+    response = client.get(
+        '/statistics/calculations-per-day',
+        headers=headers,
+        params={
+            'calc_type': 'addition',
+        },
+    )
+    assert response.status_code == 200
+    data = response.json()
+
+    assert len(data) == 8
+    expected_counts = [6, 0, 0, 5, 4, 3, 2, 1]
+    for i in range(len(data)):
+        assert data[i]['calc_date'] + ' 00:00:00' == midnight_n_days_ago(len(data) - i - 1)
+        assert data[i]['type'] == 'addition'
+        assert data[i]['count'] == expected_counts[i]
+
+    results = db_session.query(CalcsPerDay).where(CalcsPerDay.user_id == user_data['user_id']).all()
+    assert(len(results)) == 8
+
+
+def test_statistics_calcs_per_day_multiplication(db_session):
+
+    # Test CalcsPerDay with multiplication
+
+    # Create calculations
+    user_data = register_and_login(client)
+    create_calcs_by_day(user_data, db_session, client)
+
+    # Get calculations
+    access_token = user_data['access_token']
+    headers = {'Authorization': f'Bearer {access_token}'}
+    response = client.get(
+        '/statistics/calculations-per-day',
+        headers=headers,
+        params={
+            'calc_type': 'multiplication',
+        },
+    )
+    assert response.status_code == 200
+    data = response.json()
+
+    assert len(data) == 15
+    expected_counts = [20, 0, 0, 0, 0,
+                       0, 0, 0, 0, 0,
+                       0, 30, 0, 8, 7]
+    for i in range(len(data)):
+        assert data[i]['calc_date'] + ' 00:00:00' == midnight_n_days_ago(len(data) - i)
+        assert data[i]['type'] == 'multiplication'
+        assert data[i]['count'] == expected_counts[i]
+
+    results = db_session.query(CalcsPerDay).where(CalcsPerDay.user_id == user_data['user_id']).all()
+    assert(len(results)) == 15
+
+
+def test_statistics_calcs_per_day_no_results(db_session):
+
+    # Test CalcsPerDay with division (no results)
+
+    # Create calculations
+    user_data = register_and_login(client)
+    create_calcs_by_day(user_data, db_session, client)
+
+    # Get calculations
+    access_token = user_data['access_token']
+    headers = {'Authorization': f'Bearer {access_token}'}
+    response = client.get(
+        '/statistics/calculations-per-day',
+        headers=headers,
+        params={
+            'calc_type': 'division',
+        },
+    )
+    assert response.status_code == 200
+    data = response.json()
+    assert len(data) == 0
+
+    results = db_session.query(CalcsPerDay).where(CalcsPerDay.user_id == user_data['user_id']).all()
+    assert(len(results)) == 0
+
+
+def test_statistics_calcs_per_day_all_calc_types(db_session):
+
+    # Test CalcsPerDay for all calc types
+
+    # Create calculations
+    user_data = register_and_login(client)
+    create_calcs_by_day(user_data, db_session, client)
+
+    # Get calculations
+    access_token = user_data['access_token']
+    headers = {'Authorization': f'Bearer {access_token}'}
+    response = client.get(
+        '/statistics/calculations-per-day',
+        headers=headers,
+    )
+    assert response.status_code == 200
+    data = response.json()
+
+    assert len(data) == 16
+    # x + y means total_addition_cals + total_multiplication_calcs
+    expected_counts = [0 + 20, 0 +  0, 0 + 0, 0 + 0, 0 + 0,
+                       0 +  0, 0 +  0, 0 + 0, 6 + 0, 0 + 0,
+                       0 +  0, 5 + 30, 4 + 0, 3 + 8, 2 + 7,
+                       1 + 0]
+    for i in range(len(data)):
+        assert data[i]['calc_date'] + ' 00:00:00' == midnight_n_days_ago(len(data) - i - 1)
+        assert data[i]['type'] == None
+        assert data[i]['count'] == expected_counts[i]
+
+    results = db_session.query(CalcsPerDay).where(CalcsPerDay.user_id == user_data['user_id']).all()
+    assert(len(results)) == 16
+
+
+def test_statistics_calcs_per_day_start_filter(db_session):
+
+    # Test CalcsPerDay with start_time filter
+
+    # Create calculations
+    user_data = register_and_login(client)
+    create_calcs_by_day(user_data, db_session, client)
+
+    # Get calculations
+    access_token = user_data['access_token']
+    headers = {'Authorization': f'Bearer {access_token}'}
+    response = client.get(
+        '/statistics/calculations-per-day',
+        headers=headers,
+        params={
+            'calc_type': 'addition',
+            'start_time': midnight_n_days_ago(5),
+        },
+    )
+    assert response.status_code == 200
+    data = response.json()
+
+    assert len(data) == 6
+    expected_counts = [0, 5, 4, 3, 2, 1]
+    for i in range(len(data)):
+        assert data[i]['calc_date'] + ' 00:00:00' == midnight_n_days_ago(len(data) - i - 1)
+        assert data[i]['type'] == 'addition'
+        assert data[i]['count'] == expected_counts[i]
+
+    results = db_session.query(CalcsPerDay).where(CalcsPerDay.user_id == user_data['user_id']).all()
+    assert(len(results)) == 6
+
+
+def test_statistics_calcs_per_day_end_filter(db_session):
+
+    # Test CalcsPerDay with end_time filter
+
+    # Create calculations
+    user_data = register_and_login(client)
+    create_calcs_by_day(user_data, db_session, client)
+
+    # Get calculations
+    access_token = user_data['access_token']
+    headers = {'Authorization': f'Bearer {access_token}'}
+    response = client.get(
+        '/statistics/calculations-per-day',
+        headers=headers,
+        params={
+            'calc_type': 'addition',
+            'end_time': midnight_n_days_ago(3),
+        },
+    )
+    assert response.status_code == 200
+    data = response.json()
+
+    assert len(data) == 5
+    expected_counts = [6, 0, 0, 5, 4]
+    for i in range(len(data)):
+        assert data[i]['calc_date'] + ' 00:00:00' == midnight_n_days_ago(len(data) - i + 2)
+        assert data[i]['type'] == 'addition'
+        assert data[i]['count'] == expected_counts[i]
+
+    results = db_session.query(CalcsPerDay).where(CalcsPerDay.user_id == user_data['user_id']).all()
+    assert(len(results)) == 5
+
+
+def test_statistics_calcs_per_day_start_filter_and_end_filter(db_session):
+
+    # Test CalcsPerDay with start_time filter and end_time filter
+
+    # Create calculations
+    user_data = register_and_login(client)
+    create_calcs_by_day(user_data, db_session, client)
+
+    # Get calculations
+    access_token = user_data['access_token']
+    headers = {'Authorization': f'Bearer {access_token}'}
+    response = client.get(
+        '/statistics/calculations-per-day',
+        headers=headers,
+        params={
+            'calc_type': 'addition',
+            'start_time': midnight_n_days_ago(5),
+            'end_time': midnight_n_days_ago(3),
+        },
+    )
+    assert response.status_code == 200
+    data = response.json()
+
+    assert len(data) == 3
+    expected_counts = [0, 5, 4]
+    for i in range(len(data)):
+        assert data[i]['calc_date'] + ' 00:00:00' == midnight_n_days_ago(len(data) - i + 2)
+        assert data[i]['type'] == 'addition'
+        assert data[i]['count'] == expected_counts[i]
+
+    results = db_session.query(CalcsPerDay).where(CalcsPerDay.user_id == user_data['user_id']).all()
+    assert(len(results)) == 3
+
+
+def test_statistics_calcs_per_day_cached(db_session):
+
+    # Test CalcsPerDay with cached data in calcs_per_day table
+
+    # Create calculations
+    user_data = register_and_login(client)
+    create_calcs_by_day(user_data, db_session, client)
+
+    # Get calculations. Second iteration should trigger at least one cache hit.
+    for i in range(2):
+        access_token = user_data['access_token']
+        headers = {'Authorization': f'Bearer {access_token}'}
+        response = client.get(
+            '/statistics/calculations-per-day',
+            headers=headers,
+            params={
+                'calc_type': 'multiplication',
+            },
+        )
+        assert response.status_code == 200
+        data = response.json()
+
+        assert len(data) == 15
+        expected_counts = [20,  0, 0, 0, 0,
+                            0,  0, 0, 0, 0,
+                            0, 30, 0, 8, 7]
+        for i in range(len(data)):
+            assert data[i]['calc_date'] + ' 00:00:00' == midnight_n_days_ago(len(data) - i)
+            assert data[i]['type'] == 'multiplication'
+            assert data[i]['count'] == expected_counts[i]
+
+        results = db_session.query(CalcsPerDay).where(CalcsPerDay.user_id == user_data['user_id']).all()
+        assert(len(results)) == 15
--- tests/integration/test_statistics_schema.py
+++ tests/integration/test_statistics_schema.py
@@ -1,8 +1,10 @@
 import pytest
 
+from datetime import date
 from pydantic import ValidationError
 
-from app.schemas.statistics import TotalCalculations
+from app.schemas import CalcsPerDayResponse
+from app.schemas import TotalCalculations
 
 
 def test_schema_total_statistics_valid():
@@ -54,3 +56,75 @@ def test_schema_total_statistics_invalid_type():
 
     with pytest.raises(ValidationError, match='Type must be one of'):
         TotalCalculations(**data)
+
+
+def test_schema_calcs_per_day_valid():
+
+    # Test creating a valid CalcsPerDayResponse schema
+
+    data = {
+        'type': 'addition',
+        'calc_date': date.today(),
+        'count': 50,
+    }
+
+    calc = CalcsPerDayResponse(**data)
+    assert calc.type == data['type']
+    assert calc.calc_date == data['calc_date']
+    assert calc.count == data['count']
+
+
+def test_schema_calcs_per_day_type_is_none():
+
+    # Test CalcsPerDayResponse passes if type is None
+
+    data = {
+        'type': None,
+        'calc_date': date.today(),
+        'count': 50,
+    }
+
+    calc = CalcsPerDayResponse(**data)
+    assert calc.type == data['type']
+    assert calc.calc_date == data['calc_date']
+    assert calc.count == data['count']
+
+
+def test_schema_calcs_per_day_missing_calc_date():
+
+    # Test CalcsPerDayResponse fails if 'calc_date' is missing
+
+    data = {
+        'type': 'addition',
+        'count': 50,
+    }
+
+    with pytest.raises(ValidationError, match='required'):
+        CalcsPerDayResponse(**data)
+
+
+def test_schema_calcs_per_day_missing_count():
+
+    # Test CalcsPerDayResponse fails if 'count' is missing
+
+    data = {
+        'type': 'addition',
+        'calc_date': date.today(),
+    }
+
+    with pytest.raises(ValidationError, match='required'):
+        CalcsPerDayResponse(**data)
+
+
+def test_schema_calcs_per_day_invalid_type():
+
+    # Test CalcsPerDayResponse fails if 'type' is invalid
+
+    data = {
+        'type': 'modulus',
+        'calc_date': date.today(),
+        'count': 50,
+    }
+
+    with pytest.raises(ValidationError, match='Type must be one of'):
+        CalcsPerDayResponse(**data)
